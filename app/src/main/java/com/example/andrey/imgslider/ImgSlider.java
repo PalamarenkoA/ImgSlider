65922e323f1c8940e13053f20dddfe47267a499d
andrey <bahvalll@gmail.com>
2016-01-14 15:27:31 +0200

    0.1 Добавил slider

--- app/src/main/java/com/example/andrey/imgslider/ImgSlider.java
+++ app/src/main/java/com/example/andrey/imgslider/ImgSlider.java
@@ -8,13 +8,83 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.daimajia.slider.library.R.anim;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 
-public class ImgSlider extends AppCompatActivity {
+import java.util.HashMap;
+
+public class ImgSlider extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
+
+
+    SliderLayout sliderShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_img_slider);
+        toolbarAndFab();
+        sliderShow = (SliderLayout) findViewById(R.id.slider);
+        sliderShow.setDuration(1000);
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+        sliderSet(sliderShow,url_maps);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void sliderSet(SliderLayout sliderLayout, HashMap <String,String> hashMap){
+    for(String name : hashMap.keySet()){
+        TextSliderView textSliderView = new TextSliderView(this);
+        // initialize a SliderLayout
+        textSliderView
+                .description(name)
+                .image(hashMap.get(name))
+                .setScaleType(BaseSliderView.ScaleType.Fit)
+                .setOnSliderClickListener(this);
+
+        //add your extra information
+        textSliderView.bundle(new Bundle());
+        textSliderView.getBundle()
+                .putString("extra", name);
+        sliderLayout.addSlider(textSliderView);
+}}
+    private void toolbarAndFab(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,7 +97,6 @@ public class ImgSlider extends AppCompatActivity {
             }
         });
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +117,25 @@ public class ImgSlider extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
     }
 }
